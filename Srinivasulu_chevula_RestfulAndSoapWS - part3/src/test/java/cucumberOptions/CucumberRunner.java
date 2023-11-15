24ee3a2276f01d90979270b1bc5b2c85c7fd0d84
srinivas225 <53026836+srinivas225@users.noreply.github.com>
2023-11-15 16:53:47 +0530

    Update CucumberRunner.java

--- Srinivasulu_chevula_RestfulAndSoapWS - part3/src/test/java/cucumberOptions/CucumberRunner.java
+++ Srinivasulu_chevula_RestfulAndSoapWS - part3/src/test/java/cucumberOptions/CucumberRunner.java
@@ -2,14 +2,17 @@ package cucumberOptions;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
+@RunWith(JUnitPlatform.class)
+@SelectPackages("stepdefinitions")  // Package where your step definitions are located
 @CucumberOptions(
     features = "src/test/resources/features", // Path to your feature files
-    glue = "stepdefinitions", // Package or class where your step definitions are located
-    //tags="@all",
-    plugin = {"pretty","html:target/cucumberReport.html","json:target/cucumberjson-report.json"} // Specify the JSON output format
+    //tags = "@all",
+    plugin = {"pretty", "html:target/cucumberReport.html", "json:target/cucumberjson-report.json"} // Specify the JSON output format
 )
 public class CucumberRunner {
+    // No need for a main method or extending any class
 }
